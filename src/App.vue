bc25c731d866d3ec498d071ac3732636f6d2ecda
add-flav1s <aflavia17_@hotmail.com>
2021-10-23 16:14:29 -0300

    First commit, creating the project

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <img alt="Vue logo" src="./assets/logo.jpg" />
+        <Counter />
+    </div>
+</template>
+
+<script>
+import Counter from "./components/Counter.vue";
+
+export default {
+    name: "App",
+    components: {
+        Counter, //
+    },
+};
+Counter;
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+.container {
+    margin: 0 auto;
+    max-width: 960px;
+}
+
+.container img {
+    width: 500px;
+    height: 300px;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="counter">0</div>
+    <div class="buttons">
+        <button>+</button>
+        <button>-</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Counter",
+};
+</script>
+
+<style scoped>
+</style>
